f48aeabd84766769da1aab0274a4e7c7255a6963
MohanRaj0310 <65529361+MohanRaj0310@users.noreply.github.com>
2021-07-22 19:56:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let js = "amazing";
+if (js === 'amazing') {
+  // alert('js is always have fun')
+}
+console.log('hii mohan')
+let bool = false;
+bool = 1;
+console.log(typeof bool)
+let undef;
+console.log(undef)
+console.log(typeof undef)
+console.log(typeof null)
+
+let age = 90;
+age = 89;
+console.log(age)
+
+const year = 1999;
+// year = 1999;
+console.log(year)
+
+// const undefin;
+let str = 'suhdue';
+console.log(str * 2);
+console.log(8 ** 2)//exponential
+let firstName = "mohan"
+let lastName = 'Raj'
+console.log(firstName + lastName)//concatenate
+console.log(firstName + ' ' + lastName)//concatenate
+
+let x = 90;
+x++;
+x--;
+x *= 2;//180
+x /= 2;//let's see
+x %= 2;
+console.log(x);
+
