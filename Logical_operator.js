7ce3804b0a61a26aba64dca3b10d846059e180cf
MohanRaj0310 <65529361+MohanRaj0310@users.noreply.github.com>
2021-07-26 18:26:22 +0530

    initial commit

---
+++ Logical_operator.js
@@ -0,0 +1,20 @@
+
+//Logical operator
+
+//mohan should have driverlicence and has goodVision and was not tired while driving
+// aim is to drive the car even if fails to meet the criteria
+
+const hasDriverLicence = true
+const hasGoodVision = false
+const isTired = false
+
+if (hasDriverLicence || hasGoodVision && !isTired) {
+  if (1){
+    console.log(0 || 1);
+    console.log(`he met the statement.. no issue to drive the vehicle`)
+    // return isTired = true;
+  }
+} else {
+  console.log(`if one of them statement is false, try to be true it`)
+}
+  
