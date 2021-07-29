8655e73078d655b365549671018ea58c858c2337
MohanRaj0310 <65529361+MohanRaj0310@users.noreply.github.com>
2021-07-29 09:19:40 +0530

    practice file have been included for testing purpose

---
+++ hereToPractise.js
@@ -0,0 +1,12 @@
+console.log('hii practise seesion')
+// alert(`this quotes from js file`)
+let $function = 'function'
+console.log($function)
+const $if = 'if'
+console.log($if)
+const _45_dir_ = 'rjfr'
+console.log(_45_dir_) 
+let PI = 3.14;
+console.log(PI)
+PI = 90
+console.log(PI)
