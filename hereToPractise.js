4d686c98a11fac737b548511779fce903301e23a
MohanRaj0310 <65529361+MohanRaj0310@users.noreply.github.com>
2021-07-29 20:18:37 +0530

    core-js is installed

--- hereToPractise.js
+++ hereToPractise.js
@@ -1,4 +1,6 @@
-console.log('hii practise seesion')
+
+//  'use strict';
+/*console.log('hii practise seesion')
 // alert(`this quotes from js file`)
 let $function = 'function'
 console.log($function)
@@ -12,3 +14,257 @@ PI = 90
 console.log(PI)
 console.log('dje')
 console.log('df')
+
+console.log(typeof true, typeof false)
+let emptyVal = undefined;
+console.log(emptyVal)
+let empty;
+console.log(empty)
+console.log(empty)
+let nullVal = null
+console.log(nullVal)//null
+console.log(typeof nullVal)///
+
+let globalVar;
+let func = function (x){
+  console.log(typeof x)
+  var x = 90;
+  globalVa = 90
+  console.log(x)
+  return 'dii'
+}
+console.log(typeof func)//function
+func();
+// console.log(x);
+console.log(globalVa)//38
+*/
+// 'use strict';
+/*
+if (1){
+  let x = 90
+  var y = 90
+  // z = 67;
+}
+console.log(y)
+// console.log(x)
+// console.log(z)
+
+let globalVar = 56;
+function func(x,y){
+  let localVar = 90
+  globalVar = 78
+  localVar = 45
+  globalVar = x
+}
+func(23);
+console.log(globalVar)
+// console.log(localVar)
+console.log(globalVar)//23
+
+const age = 18
+// age = 18;
+
+// const largeConstant;
+console.log(`string
+diorf
+dorjofjr`)
+// console.log('fruf
+// jfirif
+// kforjo')//error
+// console.log('fgtgut/n
+// /fiire')
+console.log(`dirfrjit\n\
+jiorjojot\n`)
+console.log(`it`)
+console.log('irififr\n\
+roijfijr\
+dirfre')
+let age1 = 90
+let profile = 'modfrufr'
+const standardAge = 40;
+// console.log(`${age1}`+ `${standardAge >= 90 ? standardAge : age1}`,`${standardAge = age1 >= 90 ? standardAge : age1}` )
+
+console.log(Object())//{}
+console.log(Array())//[]
+// console.log(undefined())//Uncaught TypeError: undefined is not a function
+// console.log(BigInt())//Uncaught TypeError: Cannot convert undefined to a BigInt
+// console.log(null())//Uncaught TypeError: Cannot convert undefined to a BigInt
+console.log(Number('moahn'))
+console.log(typeof NaN)//NUMBER
+
+console.log('90' < '2200')//fasle//doesn't happen type coersion
+// console.log(09 < '290920')//true........try this statement with strict mode
+console.log('90' == 90)
+console.log(console.log())//undefined
+console.log(console.log('duu'))
+
+let x = 90;
+x *= 2;
+x +=23;
+x-=90;//expression
+console.log(x)
+
+console.log(Boolean(undefined))
+console.log(Boolean(0))
+console.log(Boolean(null))
+console.log(Boolean(``))
+console.log(Boolean(''))
+
+let age3 = 90;
+if (age3 > 18){
+  if (age3 == 18){
+    console.log(`wait for 6 month for validating`)
+  } else {
+    // return `jurf`
+    console.log(`apply asp for license`)
+  }
+} else {
+  console.log(`should be turned 18. go away`)
+  age3 = 18;
+}
+
+if ('str' === "str"){
+  console.log(1)
+  // unreferVar = 90
+}
+function func2(){
+  // unreferVar = 78;
+}
+// console.log(unreferVar)//90
+func2()
+// console.log(unreferVar)//78
+
+if (1){
+  function func2(){
+    unreferVar = 56;
+    let d = 78
+    var df= 90
+  }
+}
+func2()
+// console.log(unreferVar)//56
+// console.log(df)//undefined
+*/
+/*
+console.log('18' ==18, Number() == String())//*************** *//*true true
+console.log('18' === 18, Number() === String())
+
+const constant = 304;
+
+if (x = constant === typeof 90){
+  console.log('extremly coded ')
+} else {
+  console.log(`so, statement wrong`)
+}
+console.log(x)//false
+
+// const num = Number(prompt('enter num'))
+// console.log(num)//34
+
+console.log(typeof null)
+console.log(typeof Object)//function************************8
+
+const obj = {
+  age: 'djij'
+}
+
+console.log(typeof obj)
+console.log(typeof(typeof(obj)))//string doubt statement********
+
+const interChangeVar = 90;
+// let interChangeVar1 = undefined(interChangeVar)
+console.log(undefined)
+console.log(null)
+console.log(NaN)
+console.log('')
+console.log(Number)
+console.log(Array)
+console.log(Object)
+console.log(Boolean);
+// console.log(function)// Uncaught SyntaxError: Unexpected token ')'don't know why's happening
+
+let num = 90
+
+switch (num){
+  case 90:
+    // const alertVal = alert(y = 'dde')//hereToPractise.js:193 Uncaught TypeError: Assignment to constant variable.
+    // console.log(alertVal)//undefined
+    // x = prompt('num');
+    // console.log(x)//working
+    // console.log(y)//none 
+    //if switch has not anything, prints undefined
+    // const alertVal = alert('druf')
+    // console.log(alertVal)//undefined
+    // let alertLet = alert('fhruuf')
+    // console.log(alertLet)//undefined
+    break;
+}
+
+let age4 = 56
+
+const isEligible = age4 >= 18 ? console.log(`u r eligible to vote`) : console.log(`don't apply for ${18 - age}`)
+console.log(isEligible)//undefined
+
+function logger (x){
+  var a = x;
+  console.log(a)//67
+  console.log(x)//67
+}
+
+logger(67);
+console.log(typeof logger)//function
+
+// arrowfunction
+
+const age44 = 90;
+
+// birtheyear(3902)//cannot access
+
+const birtheyear  = (nowYear, x) => {
+  console.log(x = (age44 - nowYear))
+  console.log(x)///= lower precedence
+  console.log(x = age44 - nowYear)
+  console.log(x)//********* */
+/*}
+birtheyear(1947)
+let c = 76784;
+const newVal = 363;
+var T = 392;
+let inner2;
+
+function funcOld (x, y1){
+  let c = 78;
+  const newVal = 67;
+  var T = 34;
+  inner = 89;
+  funcNew(909)
+  function funcNew (u, y){
+    let c = 45;
+    const newVal = 12;
+    var T = 36577;
+    inner = 67;
+    // x = u
+    T = u;
+  }
+  inner = c
+  let j = 456
+  inner2 = j
+  // inner2 = x;
+  y1 = inner;
+  inner = y1
+}
+funcOld(345,678)
+// funcNew()//is not defined
+// funcOld(funcNew())//funcNew even is not defined
+console.log(inner)//89
+console.log(c)//767884
+console.log(c)
+console.log(inner2)
+// console.log(y1)//NOT DEFINED
+console.log(inner)*/
+
+// Array
+let age = 17
+const years = [13,122,`${age}`,129930, age > 18 ? 'addad': console.log('difrif'), [23,'dssd', true, !false, age++, ++age, --age, age--, console.log('nested array')],console.log('wow js is awazing')]
+console.log(typeof Array)
+console.log(Array)
