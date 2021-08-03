6dae51fb76be2c7b516f26e09ba14154c2b5f8b5
MohanRaj0310 <65529361+MohanRaj0310@users.noreply.github.com>
2021-08-03 12:46:39 +0530

    continue & break on loop

--- LoopArray_Break_con.js
+++ LoopArray_Break_con.js
@@ -49,3 +49,71 @@ emptyArray[6] = '6'
 console.log(emptyArray)
 console.log(emptyArray.indexOf(7))//7
 */
+
+/******************strict and lose not equal operator */
+
+// console.log(90 != '90')//false
+console.log(90 !== '90')//true
+console.log(90 != '90')//false
+console.log('string' !== 'string')//false
+console.log('string' != 'string')//false
+console.log('opop' === 'cjjicr')//false
+console.log('opop' === 'opop')//true
+console.log('op' == 'op')//true
+console.log('opioie' == 'dede')//false
+
+/********strict not equality operator*/
+const a = 2, b = 'hello';
+console.log()
+// strict not equal operator
+console.log(a !== 2); // false
+console.log(a !== '2'); // true
+console.log(b !== 'Hello'); // true
+console.log(90 !== '90')//true
+
+/*************losing not equality operator */
+console.log(`!=`)
+const c = 3, d = 'hello';
+
+// not equal operator
+console.log(c != 2); // true
+console.log(d != 'Hello'); // true
+console.log('hello' == 'Hello', 'Hello' === 'hello')//false, false
+console.log(90 != '90')//false
+
+/***************************continue */
+
+const array = [
+  
+  22,
+  true,
+  false,
+  'mohanraj',
+  2021-1999,
+  'bangalore',
+  [233, `console.log('logging something')`, false, !true],
+  console.log('hey developer console'),
+  {
+    fullName: 'mohanraj',
+  },
+]
+//get a string type only from an array and should be stored that in an new array
+let newArray = [];
+
+for (i = 0; i < array.length; i++){
+  if (typeof array[i] !== 'string') continue;
+  if (typeof array[i] === 'object') {
+    break
+    // return console.log('im from object array')//Uncaught SyntaxError: Illegal return statement
+  }
+  newArray.push(array[i])
+}
+console.log(newArray)
+console.log(i)
+// console.log(x)//true
+
+if (0) {
+  // return //Uncaught SyntaxError: Illegal return statement
+} else {
+  // break;//Uncaught SyntaxError: Illegal break statement
+}
