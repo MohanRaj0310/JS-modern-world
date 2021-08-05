9d192cb8c427a7e537897359a765f90e8c73c9e8
MohanRaj0310 <65529361+MohanRaj0310@users.noreply.github.com>
2021-08-05 06:23:52 +0530

    challenges are solved

--- realChallenge.js
+++ realChallenge.js
@@ -76,7 +76,149 @@ for (i = 1; i <= promptVal; i++) {
 // console.log(even)
 // console.log(add)
 
-/************************Web wrokers */
+// Write a program that prompts the user to input a positive integer. It should then output a message indicating whether the number is a prime number. 
 
-console.log(typeof Worker)//
+/*********for one value */
+// let x;
+// function primeNum(int) {
+//   let i = 1;
+//   tempprime = [];
+//   while (i <= int) {
+//     if (int % i === 0) {
+//       tempprime.push(i)
+//     }
+//     i++;
+//   }
+// }
+// primeNum(23)
+// console.log(tempprime)
 
+// for mULTIPLE value
+
+// let num = [90];
+// let num_0 = []
+// let num_1 = []
+// let num_2 = []
+// let num_3 = []
+// let num_4 = []
+// let num_5 = []
+
+// for (let i = 0; i < num.length; i++) {
+//   for (let j = 1; j <= num[i]; j++) {
+//     if (num[i] % j === 0) {
+//       num_0.push(j)
+//       // console.log(`divisor of ${num[i]} are j}`)
+//     }
+//   }
+// }
+// console.log(num_0)
+
+/*******finding divisors */
+// function divisors(integer) {
+
+//   var tmp = [];
+//   for (var i = 2; i < integer; i++) {
+//     if (integer % i == 0) {
+//       tmp.push(i);
+//     }
+
+//   }
+//   if (tmp.length == 0) {
+//     return integer + " is prime"
+//   } else {
+//     return tmp
+//   }
+
+// };
+// console.log(divisors(45))
+
+/**************highest common factor  */
+// ex: 4 --> 1, 2, 4
+// ex: 10 --> 1, 2, 5
+// highest common factor is 1,2
+
+// function hcf(x, y) {
+//   let arg_1 = []
+//   let arg_2 = []
+//   console.log(`${x}'s factors are following`)
+//   for (let i = 1; i <= arg_1; i++) {
+//     if (arg_1 % i == 0) {
+//       arg_1.push(i);
+//     }
+//   }
+//   for (let i = 1; i <= arg_2; i++) {
+//     if (arg_2 % i == 0) {
+//       console.log(i)
+//       arg_2.push(i)
+//     }
+//   }
+//   let i = 0;
+//   let j = 1;
+//   let hcf = [];
+//   while (arg_2[j] < arg_1.length) {
+//     if (arg_1[i] === arg_2[j]) {
+//       hcf.push(arg_1[i])
+//     }
+//     i++;
+//     j++;
+//   }
+//   return arg_2, arg_1;
+// }
+// let temp = [];
+// let h_factor;
+// console.log(hcf(4, 8))
+// const hcf1 = (x, y) => {
+//   for (i = 1; i <= x && i <= y; i++) {
+//     if (x % i == 0 && y % i == 0) {
+//       temp.push(sorter(i))
+//     }
+//   }
+//   function sorter(i) {
+//     while (i >= 200) {
+//       return i
+//     }
+//   }
+// }
+// console.log(hcf1(400, 200))
+// console.log(temp)
+// console.log(temp)
+
+// let hcf;
+// // take input
+// const number1 = prompt('Enter a first positive integer: ');
+// const number2 = prompt('Enter a second positive integer: ');
+
+// // looping from 1 to number1 and number2
+// for (let i = 1; i <= number1 && i <= number2; i++) {
+
+//   // check if is factor of both integers
+//   if (number1 % i == 0 && number2 % i == 0) {
+//     hcf = i;
+//   }
+// }
+
+// // display the hcf
+// console.log(`HCF of ${number1} and ${number2} is ${hcf}.`);
+
+/********************** program to find the HCF or GCD of two integers*/
+
+// take input
+// let number1 = prompt('Enter a first positive integer: ');
+// let number2 = prompt('Enter a second positive integer: ');
+// let number1 = 200;
+// let number2 = 400;
+
+// // looping until both numbers are equal
+// while (number1 != number2) {
+//   if (number1 > number2) {
+//     number1 -= number2;
+//   }
+//   else {
+//     number2 -= number1;
+//   }
+// }
+
+// // display the hcf
+// console.log(`HCF is ${number1}`);
+
+// // console.log(45, prompt(23))
