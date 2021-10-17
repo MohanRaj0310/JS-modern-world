5d412a23370bb8009bdc1465b44a1557480a61bf
MohanRaj0310 <65529361+MohanRaj0310@users.noreply.github.com>
2021-10-17 14:27:03 +0530

    advance javascript

--- karl.js
+++ karl.js
@@ -1,37 +1,56 @@
-let str = 'how"re" interpreted';
-str = "it's mohanraj";
-console.log(str);
-console.log(str);
+// let str = 'how"re" interpreted';
+// str = "it's mohanraj";
+// console.log(str);
+// console.log(str);
 
-x = console.log(true + false); //1
-console.log("hi this\"s mohanraj")///*********************\\\\\\\\\\\\\\ */
+// x = console.log(true + false); //1
+// console.log("hi this\"s mohanraj")///*********************\\\\\\\\\\\\\\ */
 
-//precedence with letters
-console.log('a' < 'z')
-console.log('c' > 's')
-console.log('iicriicr' > 'drufufu')
+// //precedence with letters
+// console.log('a' < 'z')
+// console.log('c' > 's')
+// console.log('iicriicr' > 'drufufu')
 
-///null vs defined
+// ///null vs defined
 
-console.log(undefined == null)//true
-console.log(undefined != null)//false
-console.log(undefined === null)
-console.log(undefined != null)
-console.log(undefined !== null)
-console.log(null + 'dieie')
-console.log(null)
-console.log(null+23)//23************
-console.log(null + undefined)//NaN
-console.log(23+null)//23
-console.log(23+undefined)//NaN
-console.log(undefined + 23)//NaN
-console.log(null + false)//0*********
-console.log(undefined + false)//NaN
-console.log(true + null)//1
-console.log(true + undefined)//NaN
-console.log(null == false)//false
-console.log(undefined == false)//false
-console.log(undefined == false)//false
+// console.log(undefined == null)//true
+// console.log(undefined != null)//false
+// console.log(undefined === null)
+// console.log(undefined != null)
+// console.log(undefined !== null)
+// console.log(null + 'dieie')
+// console.log(null)
+// console.log(null+23)//23************
+// console.log(null + undefined)//NaN
+// console.log(23+null)//23
+// console.log(23+undefined)//NaN
+// console.log(undefined + 23)//NaN
+// console.log(null + false)//0*********
+// console.log(undefined + false)//NaN
+// console.log(true + null)//1
+// console.log(true + undefined)//NaN
+// console.log(null == false)//false
+// console.log(undefined == false)//false
+// console.log(undefined == false)//false
 
-// comment out starts with ctrl + u
+// // comment out starts with ctrl + u
+
+// var keyword
+
+let a = 90
+
+a += 120// a = a + 120
+console.log(a)
+
+a *=90// a = a(210) * 90
+console.log(a)
+
+let num = Boolean(true)
+
+// console.log(num)
+
+//truthy value
+// "wd",1,2456 except 0, 90.89, true
+/**************falsy value */
+// 0, undefined, null, '', "", false
 
